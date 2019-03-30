0a7bff455fe3fca06b94bbd0a0b1f2492f3d8f0f
CynicalToaster <danevans485@gmail.com>
2019-03-30 00:11:49 +0000

    Update App.vue

--- vue/src/cyns-kustom-app/App.vue
+++ vue/src/cyns-kustom-app/App.vue
@@ -1,10 +1,5 @@
 <template>
   <shell>
-    <h2>Subdomains</h2>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </shell>
 </template>
