a5520d990fcc364db8d7a46aa07e4b4844939e87
CynicalToaster <danevans485@gmail.com>
2019-05-16 21:26:12 +0100

    Fixed core app

--- vue/src/teedev/App.vue
+++ vue/src/teedev/App.vue
@@ -1,8 +1,6 @@
 <template>
   <shell>
-    <component :is="layout">
-      <router-view />
-    </component>
+    <router-view />
   </shell>
 </template>
 
@@ -11,35 +9,11 @@ import '@/global/assets/scss/core.scss';
 
 import Shell from '@/core/components/Shell';
 
-import NoneLayout from '@/global/layouts/None';
-import StandardLayout from '@/global/layouts/Standard';
-// import domCollector from '@/core/dom-collector';
-
 export default {
   name: 'app',
 
   components: {
     Shell,
-    NoneLayout,
-    StandardLayout,
   },
-
-  computed: {
-    layout() {
-      const pageLayout = this.$store.state.route.meta.layout;
-
-      if (pageLayout) {
-        return `${pageLayout}-layout`;
-      }
-
-      return 'none-layout';
-    },
-  },
-
-  // mounted() {
-  //   setTimeout(() => {
-  //     console.log(domCollector.capture());
-  //   }, 1000);
-  // },
 };
 </script>
--- vue/src/teedev/main.js
+++ vue/src/teedev/main.js
@@ -2,16 +2,11 @@ import { Boot } from '@/core';
 import App from './App.vue';
 import '@/global';
 
-import '@/teemill/pages';
+import './pages';
 
+// What the fuck is this Charles?
 // import { Guards as GlobalGuards } from '@/global/router';
 // import { Guards as LocalGuards } from '@/teemill/router';
-
-// import Plugins from '@/teemill/plugins';
-
-// console.log(GlobalGuards);
-// console.log(LocalGuards);
-// console.log(Plugins);
 
 /**
  * Bootstrap the app.
