b158558609b8f3f36434f678d8587e24af860738
DanEvans485 <dan.e@teemill.com>
2019-08-04 13:55:43 +0100

    chore: Added path aliases to vue config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   configureWebpack: {
     plugins: [
@@ -13,5 +17,11 @@ module.exports = {
       preProcessor: 'scss',
       patterns: [path.resolve(__dirname, 'src/assets/scss/global.scss')]
     }
-  }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('@gbro', resolve('src/users/gbro'))
+      .set('@devan', resolve('src/users/devan'))
+  },
 };
