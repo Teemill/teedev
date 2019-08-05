c0739dc9681e4806f6a31f67ba223cc9ff911e3b
DanEvans485 <dan.e@teemill.com>
2019-08-05 22:15:17 +0100

    feat(component): Ratio component

---
+++ src/components/containers/Ratio.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="t-ratio relative"
+    :style="{
+      paddingBottom: `${ratio * 100}%`,
+    }"
+  >
+    <div
+      class="
+        absolute
+        top-0
+        left-0
+        bottom-0
+        right-0
+      "
+    >
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 't-ratio',
+
+  props: {
+    ratio: {
+      type: Number,
+      default: 1,
+    },
+  },
+});
+</script>
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as tRatio } from './containers/Ratio.vue';
