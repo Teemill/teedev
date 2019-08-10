699201b5e38d413a4e7f2623010736c314a055b4
DanEvans485 <dan.e@teemill.com>
2019-08-10 14:30:14 +0100

    chore: Added transform import plugin

--- babel.config.js
+++ babel.config.js
@@ -7,4 +7,19 @@ module.exports = {
       },
     ],
   ],
+  plugins: [
+    [
+      'transform-imports',
+      {
+        'lodash': {
+          transform: 'lodash/${member}',
+          preventFullImport: false,
+        },
+        'element-ui': {
+          transform: importName => `element-ui/lib/${importName.toLowerCase()}`,
+          preventFullImport: false,
+        },
+      },
+    ],
+  ],
 };
