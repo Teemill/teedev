a9c3bda8886c91bc34e989f7945391c9876049e3
DanEvans485 <dan.e@teemill.com>
2019-08-17 15:51:15 +0100

    chore: Added faker

---
+++ src/search.ts
@@ -0,0 +1,56 @@
+const inputString: string = 'The quick brown bat jumped over the fat rat.';
+const searchString: string = 'quake';
+
+function levenshtein(a: string, b: string) {
+  const aLength: number = a.length;
+  const bLength: number = b.length;
+
+  if (aLength === 0) {
+    return bLength;
+  }
+ 
+  if (bLength === 0) {
+    return aLength;
+  }
+
+  const matrix: number[][] = [];
+
+  let i:  number = 0;
+  let j:  number = 0;
+  let bi: number = 0;
+  let bj: number = 0;
+
+  for(i = 0; i <= bLength; ++i){
+    matrix[i] = [i];
+  }
+
+  for(i = 0; i <= aLength; ++i){
+    matrix[0][i] = i;
+  }
+
+  for (i = 1; i <= bLength; ++i) {
+    bi = i - 1;
+
+    for (j = 1; j <= aLength; ++j) {
+      bj = j - 1;
+
+      if (b.charAt(bi) === a.charAt(bj)) {
+        matrix[i][j] = matrix[bi][bj];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[bi][bj] + 1,
+          Math.min(
+            matrix[i][bj] + 1,
+            matrix[bi][j] + 1,
+          ),
+        );
+      }
+    }
+  }
+
+  return matrix[bLength][aLength];
+};
+
+console.log(levenshtein('what', 'test'));
+
+window.test = levenshtein;
--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -1,5 +1,88 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="onClick(1)">1 Iteration</button>
+    <button @click="onClick(100)">100 Iterations</button>
+    <button @click="onClick(10000)">10000 Iterations</button>
+    <button @click="onClick(1000000)">1000000 Iterations</button>
+    <button @click="onClick(10000000)">10000000 Iterations</button>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import faker from 'faker';
+
+import { Dot } from '@teedev/core';
+
+import '@/search.ts';
+
+export default Vue.extend({
+  name: 'page-about',
+
+  data: () => ({
+    data: [] as any[],
+  }),
+
+  methods: {
+    createData(amount: number) {
+      for (let i = 0; i < amount; i++) {
+        this.data.push({
+          id: i,
+          name: faker.name.findName(),
+          email: faker.internet.email(),
+          job: faker.name.jobTitle(),
+          description: faker.random.words(10),
+          test: {
+            first: 'test123',
+          },
+          options: [
+            {
+              name: faker.finance.accountName(),
+            },
+            {
+              name: faker.finance.accountName(),
+            },
+            {
+              name: faker.finance.accountName(),
+            },
+          ],
+        });
+      }
+    },
+
+    onClick(count: number) {
+      this.createData(10);
+
+      let t = performance.now();
+      console.log(Dot.getMany(
+        [
+          'name',
+          'description',
+          'test.first',
+        ],
+        this.data,
+      ).flat());
+      console.log('Time', `${(performance.now() - t).toFixed(2)}ms`);
+
+      let value = 0;
+      t = performance.now();
+
+      for (let i = 0; i < count; ++i) {       
+        value = window.test('save our seas', 'save');
+      }
+
+      console.log('Time', `${(performance.now() - t).toFixed(2)}ms`);
+      console.log('Value', value);
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+button {
+  padding: 1em;
+  margin: 0.5em;
+  border: 1px solid;
+}
+</style>
