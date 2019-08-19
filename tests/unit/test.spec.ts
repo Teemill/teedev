1bfa1127275c433eb622b03eb127e5a8863b4a4b
DanEvans485 <dan.e@teemill.com>
2019-08-19 22:08:31 +0100

    Update test.spec.ts

--- tests/unit/test.spec.ts
+++ tests/unit/test.spec.ts
@@ -2,6 +2,6 @@ import { expect } from 'chai';
 
 describe('Test UnitTest', () => {
   it('just an example unit test', () => {
-    expect(true).to.equal(true);
+    expect(true).to.equal(false);
   });
 });
