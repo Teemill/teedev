0b9713dfc9221c0185e4b4b597d8a2f4e292af1d
DanEvans485 <dan.e@teemill.com>
2019-08-19 22:20:31 +0100

    chore: Work u bitch

--- tests/unit/test.spec.ts
+++ tests/unit/test.spec.ts
@@ -4,4 +4,7 @@ describe('Test UnitTest', () => {
   it('just an example unit test', () => {
     expect(true).to.equal(true);
   });
+  it('just an example unit test', () => {
+    expect(true).to.equal(true);
+  });
 });
