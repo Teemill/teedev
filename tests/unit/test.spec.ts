3185a0c7c8f7d0f1abe76d5c8d3ea3fc65a0bbab
DanEvans485 <dan.e@teemill.com>
2019-08-19 22:22:26 +0100

    chore: >:( But y

--- tests/unit/test.spec.ts
+++ tests/unit/test.spec.ts
@@ -7,4 +7,7 @@ describe('Test UnitTest', () => {
   it('just an example unit test', () => {
     expect(true).to.equal(true);
   });
+  it('just an example unit test', () => {
+    expect(true).to.equal(true);
+  });
 });
